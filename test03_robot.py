0a143fdcf4594280c4cfaa9c9642389c9126aaf1
dorebom <dorebom.b@gmail.com>
2020-08-02 21:57:44 +0900

    Add: robot

--- test03_robot.py
+++ test03_robot.py
@@ -20,5 +20,35 @@ if __name__ == '__main__':
     orn_q = p.getQuaternionFromEuler(base_pose[3:6])
     robot_id = p.loadURDF("urdf/kuka_iiwa_6/basic_model.urdf", basePosition=base_pose[:3], baseOrientation=orn_q)
 
+    #p.resetJointState(robot_id, 3, targetValue=np.pi * 0.25)
+    p.stepSimulation()
+
+    print('robot joint num:',p.getNumJoints(robot_id))
+
+    end_effector_link_index = 6
+    wrist_link_index = 4
 
 
+    states = p.getLinkState(robot_id, end_effector_link_index)
+    act_abs_tcp_pose = np.zeros(6)
+    act_abs_tcp_pose[:3] = states[0]
+    act_abs_tcp_pose[3:6] = p.getEulerFromQuaternion(states[1])
+    print("pos_tcp: ", act_abs_tcp_pose[:3])
+    print("orn_tcp: ", act_abs_tcp_pose[3:6])
+
+    states = p.getLinkState(robot_id, wrist_link_index)
+    act_abs_tcp_pose = np.zeros(6)
+    act_abs_tcp_pose[:3] = states[4]
+    act_abs_tcp_pose[3:6] = p.getEulerFromQuaternion(states[5])
+
+    print("pos_w: ", act_abs_tcp_pose[:3])
+    print("orn_w: ", act_abs_tcp_pose[3:6])
+
+    act_joint_pos = np.zeros(6)
+    for i in range(6):
+        act_joint_pos[i] = p.getJointState(robot_id, i)[0]
+    print('j_pos: ', act_joint_pos)
+
+
+
+    time.sleep(3)
